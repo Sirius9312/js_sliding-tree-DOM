7f214660b95b90edf1e835911da054534b77077e
Sergey Arnautov <sirius9312@ukr.net>
2024-01-15 09:41:17 +0200

    Updates

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const menu = document.querySelector(".tree");
+const items = document.querySelectorAll("li");
+[
+    ...items
+].map((item)=>{
+    const span = document.createElement("span");
+    item.prepend(span);
+    span.append(span.nextSibling);
+});
+menu.addEventListener("click", (e)=>{
+    const item = e.target;
+    if (item.parentNode.children[1]) item.parentNode.children[1].hidden = !item.parentNode.children[1].hidden;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
